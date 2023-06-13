fd868f5832d310c9c6243d1b57b0f5b63aad7851
sgrubas <serafimgrubas@gmail.com>
2023-06-12 21:05:53 -0600

    new reading function based on `obspy.read`

--- cats/io/__init__.py
+++ cats/io/__init__.py
@@ -1,3 +1,19 @@
-from .mat import write_mat, read_mat
-# from .segy import *
-# from .mseed import *
+from .mat import write_mat, read_mat, HDRC_FIELDS_INFO
+from obspy import read
+import numpy as np
+from pathlib import Path
+
+
+def read_data(path):
+    path = Path(path)
+    if path.name[-4:] == '.mat':
+        return read_mat(path)
+    else:
+        return _read_by_obspy(path)
+
+
+def _read_by_obspy(path):
+    stream = read(path)
+    data_dict = {'data': np.array([tr.data for tr in stream]),
+                 'stats': getattr(stream, 'stats', None)}
+    return data_dict
