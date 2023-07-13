bfab9b377164598293cc30bbd550453976800454
sgrubas <serafimgrubas@gmail.com>
2023-07-12 20:56:30 -0600

    + replaced old functions for filtering intervals with more concise + slight modification of GiveIntervals function, minor change in structure

--- cats/core/projection.py
+++ cats/core/projection.py
@@ -50,21 +50,15 @@ def _giveIntervals(detection):
     return intervals[:j]
 
 
-@nb.njit("List(i8[:, :])(b1[:, :])", cache=True)
-def _giveIntervalsN(detection):
-    intervals = []
-    for di in detection:
-        intervals.append(_giveIntervals(di))
+def GiveIntervals(detection, /):
+    intervals = np.empty(detection.shape[:-1], dtype=object)
+    for i, _ in np.ndenumerate(intervals):
+        intervals[i] = _giveIntervals(detection[i])
     return intervals
 
 
-@ReshapeArraysDecorator(dim=2, input_num=1, methodfunc=False, output_num=0, first_shape=True)
-def GiveIntervals(detection, /):
-    return np.array(_giveIntervalsN(detection).astype(bool), dtype=object)
-
-
 @nb.njit("b1[:, :](b1[:, :], i8, i8)", cache=True)
-def _removeGaps(detection, min_separation, min_width):
+def _removeGaps_OLD(detection, min_separation, min_width):
     M, Nt = detection.shape
     filtered = np.full_like(detection, False)
     for i in nb.prange(M):
@@ -90,9 +84,47 @@ def _removeGaps(detection, min_separation, min_width):
     return filtered
 
 
-@ReshapeArraysDecorator(dim=2)
-def RemoveGaps(detection, /, min_separation, min_width):
-    return _removeGaps(detection, min_separation, min_width)
+@nb.njit(["f8[:, :](f8[:, :], f8, f8)", "f4[:, :](f4[:, :], f8, f8)",
+          "i8[:, :](i8[:, :], i8, i8)", "i4[:, :](i4[:, :], i8, i8)"],
+         cache=True, parallel=True)
+def filter_intervals(intervals, min_separation, min_duration):
+    # subsequent start - preceding end
+    combine_inds = (intervals[1:, 0] - intervals[:-1, 1]) >= min_separation
+    combine_inds = np.concatenate((np.array([True]),
+                                   combine_inds,
+                                   np.array([True])))  # include first start and last end
+
+    combined_intervals = np.stack((
+                                   intervals[:, 0][combine_inds[:-1]],  # starts with separation `> min` from preceding
+                                   intervals[:, 1][combine_inds[1:]]  # ends with separation `> min` from subsequent
+                                  ), axis=-1)
+    duration_inds = (combined_intervals[:, 1] - combined_intervals[:, 0]) >= min_duration
+
+    return combined_intervals[duration_inds]
+
+
+def FilterIntervals(intervals, min_separation, min_duration):
+    for ind, intv in np.ndenumerate(intervals):
+        intervals[ind] = filter_intervals(intv, min_separation, min_duration)
+    return intervals
+
+
+@nb.njit(["b1[:](i8[:, :], b1[:])",
+          "b1[:](i4[:, :], b1[:])"], cache=True, parallel=True)
+def project_intervals(intervals, output_array):
+    for i in nb.prange(len(intervals)):
+        t1, t2 = intervals[i]
+        output_array[t1: t2 + 1] = True
+    return output_array
+
+
+def FilterDetection(detection, min_separation, min_duration):
+    intervals = np.empty(detection.shape[:-1], dtype=object)
+    filtered_detection = np.full_like(detection, False, dtype=bool)
+    for ind, _ in np.ndenumerate(intervals):
+        intervals[ind] = filter_intervals(_giveIntervals(detection[ind]), min_separation, min_duration)
+        filtered_detection[ind] = project_intervals(intervals[ind], filtered_detection[ind])
+    return filtered_detection, intervals
 
 
 @nb.njit(["b1[:](i8[:])", "b1[:](i4[:])", "b1[:](u2[:])", "b1[:](u4[:])"])
@@ -120,179 +152,3 @@ def _fill_gaps_nd(labeled_sequences):
 @ReshapeArraysDecorator(dim=2, input_num=1, output_num=1)
 def FillGaps(labeled_sequences):
     return _fill_gaps_nd(labeled_sequences)
-
-
-@nb.njit("f8[:, :](b1[:], f8[:])", cache=True)
-def _giveTimeIntervals(detection, time):
-    """
-        Extracts time intervals of `True` values from boolean array.
-
-        Arguments:
-            detection : boolean np.ndarray (N,) : boolean array, assuming `True` is earthquake, `False` is noise
-            time : float np.ndarray (N,) : time axis to assign time values to the extracted intervals
-
-        Returns:
-            intervals : np.ndarray (nt, 2) : time intervals, where `nt` is number of found intervals,
-                                             e.g. if `nt = 2` then [[0.0, 2.0], [5.0, 5.3]]
-    """
-    N = len(detection)
-    intervals = np.empty((N // 2 + 1, 2))
-    t1 = t2 = -1.0
-    j = 0
-    for i in range(N):
-        d_i = detection[i]
-        t_i = time[i]
-        if d_i and (i < N - 1):
-            if t1 < 0:
-                t1 = t_i
-            else:
-                t2 = t_i
-        elif d_i and (i == N - 1) and (t1 >= 0):
-            intervals[j, 0] = t1
-            intervals[j, 1] = t_i
-            t1 = t2 = -1.0
-            j += 1
-        elif (not d_i) and (t1 >= 0):
-            intervals[j, 0] = t1
-            intervals[j, 1] = t1 if t2 < 0 else t2
-            t1 = t2 = -1.0
-            j += 1
-    return intervals[:j]
-
-
-@nb.njit(["f8[:, :](f8[:, :], f8, f8)",
-          "i8[:, :](i8[:, :], i8, i8)"], cache=True)
-def _filterIntervals(intervals, max_gap, min_width):
-    """
-        Filters intervals by combining close intervals with gap <= `max_gap` and removing short intervals < `min_width`.
-
-        Arguments:
-            intervals : np.ndarray (nt, 2) : time intervals, where `nt` is number of found intervals,
-            max_gap : float : maximum gap between two close intervals to be considered as one big interval
-            min_width : float : minimum width of intervals to be kept
-
-        Returns:
-            filtered_intervals : np.ndarray (ntf, 2) : `ntf` filtered time intervals
-    """
-    n = len(intervals)
-    if (max_gap > 0) and (n > 0):
-        filtered = [(intervals[0, 0], intervals[0, 1])]
-        for i in range(1, n):
-            intv_new = intervals[i]
-            intv_old = filtered[-1]
-            if (intv_new[0] - intv_old[1]) > max_gap:
-                if (intv_old[1] - intv_old[0]) < min_width:
-                    filtered.pop(-1)
-                filtered.append((intv_new[0], intv_new[1]))
-            else:
-                filtered[-1] = (intv_old[0], intv_new[1])
-        if (len(filtered) > 0) and (filtered[-1][1] - filtered[-1][0]) < min_width:
-            filtered.pop(-1)
-        filtered = np.array(filtered) if len(filtered) > 0 else np.zeros((0, 2), dtype=intervals.dtype)
-    else:
-        filtered = intervals
-    
-    return filtered
-
-
-@nb.njit("List(f8[:, :])(b1[:, :], f8[:], f8, f8)", cache=True)
-def _filterTimeIntervalsN(detection, time, max_gap, min_width):
-    intervals = []
-    for di in detection:
-        intervals.append(_filterIntervals(_giveTimeIntervals(di, time), max_gap, min_width))
-    return intervals
-
-
-@nb.njit("List(i8[:, :])(b1[:, :], i8, i8)", cache=True)
-def _filterIntervalsN(detection, max_gap, min_width):
-    intervals = []
-    for di in detection:
-        intervals.append(_filterIntervals(_giveIntervals(di), max_gap, min_width))
-    return intervals
-
-
-def FilterIntervals(detection, max_gap, min_width, time=None):
-    """
-        Extracts and filters time intervals of `True` values from boolean array.
-        Filters by combining close intervals with gap <= `max_gap` and removing short intervals < `min_width`.
-
-        Arguments:
-            detection : boolean np.ndarray (M, N,) : `M` boolean arrays, assuming `True` is earthquake, `False` is noise
-            max_gap : float : maximum gap between two close intervals to be considered as one big interval
-            min_width : float : minimum width of intervals to be kept
-            time : float np.ndarray (N,) : time axis to assign time values to the extracted intervals
-
-        Returns:
-            filtered_intervals : List(np.ndarray (ntf, 2)) : `ntf` filtered time intervals
-    """
-    if time is None:
-        filtered = _filterIntervalsN(detection, int(max_gap), int(min_width))
-    else:
-        filtered = _filterTimeIntervalsN(detection, time, float(max_gap), float(min_width))
-    return filtered
-
-
-@nb.njit("b1[:](f8[:, :], i8, f8)", cache=True)
-def _projectIntervals(intervals, N, dt):
-    """
-        Projects `True` values onto time axis with sampling `dt_sec` and length `N`.
-
-        Arguments:
-            intervals : np.ndarray (nt, 2) : `nt` time intervals
-            N : int : number of elements in time axis
-            dt : int : sampling of time axis
-
-        Returns:
-            detection : boolean np.ndarray (N,) : boolean array, `True` is earthquake, `False` is noise
-    """
-    b = np.full(N, False)
-    for (t1, t2) in intervals:
-        i1, i2 = int(t1 / dt), int(np.ceil(t2 / dt)) + 1
-        b[i1: i2] = True
-    return b
-
-
-@nb.njit("b1[:, :](b1[:, :], f8[:], f8, f8, f8[:])", parallel=True, cache=True)
-def _projectFilterIntervalsN(detection, time, max_gap, min_width, new_time):
-    """
-        Filters boolean array `detection` by combining close `True` intervals with gap <= `max_gap`
-        and removing short intervals < `min_width`. Then, projects onto `new_time` axis.
-
-        Arguments:
-            detection : boolean np.ndarray (M, Nt) : `M` boolean arrays, `True` is earthquake, `False` is noise
-            time : float np.ndarray (Nt,) : time axis for extraction of intervals
-            max_gap : float : maximum gap between two close intervals to be considered as one big interval
-            min_width : float : minimum width of intervals to be kept
-            time : float np.ndarray (N,) : new time axis for projection of filtered intervals
-
-        Returns:
-            filtered_detection : boolean np.ndarray (M, N) : filtered and re-projected boolean array
-    """
-    dt = new_time[1] - new_time[0]
-    n = len(detection)
-    N = len(new_time)
-    b = np.empty((n, N), dtype=np.bool_)
-    for i in nb.prange(n):
-        intervals = _giveTimeIntervals(detection[i], time)
-        filtered = _filterIntervals(intervals, max_gap, min_width)
-        b[i] = _projectIntervals(filtered, N, dt)
-    return b
-
-
-@ReshapeArraysDecorator(dim=2, input_num=1, methodfunc=False, output_num=1, first_shape=True)
-def ProjectFilterIntervals(detection, /, time, max_gap, min_width, new_time):
-    """
-        Filters boolean array `detection` by combining close `True` intervals with gap <= `max_gap`
-        and removing short intervals < `min_width`. Then, projects onto `new_time` axis.
-
-        Arguments:
-            detection : boolean np.ndarray (..., Nt) : boolean arrays, `True` is earthquake, `False` is noise
-            time : float np.ndarray (Nt,) : time axis for extraction of intervals
-            max_gap : float : maximum gap between two close intervals to be considered as one big interval
-            min_width : float : minimum width of intervals to be kept
-            time : float np.ndarray (N,) : new time axis for projection of filtered intervals
-
-        Returns:
-            filtered_detection : boolean np.ndarray (..., N) : filtered and re-projected boolean array
-    """
-    return _projectFilterIntervalsN(detection, time, max_gap, min_width, new_time)
