ef2fdb8a052534ca0be2fcf15b7701293d1ba9ed
sgrubas <serafimgrubas@gmail.com>
2023-08-07 16:55:12 -0600

    Fixed a serious bug related to unpredictable behaviour if `len(intervals) < 2`.

--- cats/core/projection.py
+++ cats/core/projection.py
@@ -86,19 +86,26 @@ def _removeGaps_OLD(detection, min_separation, min_width):
           "i8[:, :](i8[:, :], i8, i8)", "i4[:, :](i4[:, :], i8, i8)"],
          cache=True, parallel=True)
 def filter_intervals(intervals, min_separation, min_duration):
-    # subsequent start - preceding end
-    combine_inds = (intervals[1:, 0] - intervals[:-1, 1]) >= min_separation
-    combine_inds = np.concatenate((np.array([True]),
-                                   combine_inds,
-                                   np.array([True])))  # include first start and last end
+    if len(intervals > 1):
+        # subsequent start - preceding end
+        combine_inds = (intervals[1:, 0] - intervals[:-1, 1]) >= min_separation
+        combine_inds = np.concatenate((np.array([True]),
+                                       combine_inds,
+                                       np.array([True])))  # include first start and last end
 
-    combined_intervals = np.stack((
-                                   intervals[:, 0][combine_inds[:-1]],  # starts with separation `> min` from preceding
-                                   intervals[:, 1][combine_inds[1:]]  # ends with separation `> min` from subsequent
-                                  ), axis=-1)
-    duration_inds = (combined_intervals[:, 1] - combined_intervals[:, 0]) >= min_duration - 1  # `-` is for single point
+        combined_intervals = np.stack(
+            (intervals[:, 0][combine_inds[:-1]],  # starts with separation `> min` from preceding
+             intervals[:, 1][combine_inds[1:]]  # ends with separation `> min` from subsequent
+             ), axis=-1)
+    else:
+        combined_intervals = intervals
 
-    return combined_intervals[duration_inds]
+    if len(combined_intervals) > 0:
+        duration_inds = (combined_intervals[:, 1] - combined_intervals[:,
+                                                    0]) >= min_duration - 1  # `-` is for single point
+        return combined_intervals[duration_inds]
+    else:
+        return combined_intervals
 
 
 def FilterIntervals(intervals, min_separation, min_duration):
@@ -107,22 +114,23 @@ def FilterIntervals(intervals, min_separation, min_duration):
     return intervals
 
 
-@nb.njit(["b1[:](i8[:, :], b1[:])",
-          "b1[:](i4[:, :], b1[:])"], cache=True, parallel=True)
+@nb.njit(["(i8[:, :], b1[:])",
+          "(i4[:, :], b1[:])"], cache=True, parallel=True)
 def project_intervals(intervals, output_array):
     for i in nb.prange(len(intervals)):
         t1, t2 = intervals[i]
         output_array[t1: t2 + 1] = True
-    return output_array
 
 
 def FilterDetection(detection, min_separation, min_duration):
-    intervals = np.empty(detection.shape[:-1], dtype=object)
+    filtered_intervals = np.empty(detection.shape[:-1], dtype=object)
     filtered_detection = np.full_like(detection, False, dtype=bool)
-    for ind, _ in np.ndenumerate(intervals):
-        intervals[ind] = filter_intervals(_giveIntervals(detection[ind]), min_separation, min_duration)
-        filtered_detection[ind] = project_intervals(intervals[ind], filtered_detection[ind])
-    return filtered_detection, intervals
+    for ind, _ in np.ndenumerate(filtered_intervals):
+        interval_ind = _giveIntervals(detection[ind])
+        filtered_intervals[ind] = filter_intervals(interval_ind, min_separation, min_duration)
+        project_intervals(filtered_intervals[ind], filtered_detection[ind])
+
+    return filtered_detection, filtered_intervals
 
 
 @nb.njit(["b1[:](i8[:])", "b1[:](i4[:])", "b1[:](u2[:])", "b1[:](u4[:])"])
