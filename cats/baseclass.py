ab5536d835e155034a150c8b3faa8ad0a86665f6
sgrubas <serafimgrubas@gmail.com>
2023-09-20 10:12:42 -0600

    refactored the base class, mainly made base function `_apply` as separate methods for each step

--- cats/baseclass.py
+++ cats/baseclass.py
@@ -16,8 +16,8 @@ from .core.timefrequency import STFTOperator
 from .core.clustering import Clustering, ClusterCatalogs, concatenate_arrays_of_cluster_catalogs
 from .core.date import BEDATE_trimming, group_frequency, bandpass_frequency_groups
 from .core.env_variables import get_min_bedate_block_size, get_max_memory_available_for_cats
-from .core.utils import get_interval_division, format_index_by_dimensions, cast_to_bool_dict, StatusKeeper
-from .core.utils import format_interval_by_limits, give_index_slice_by_limits, del_vals_by_keys, update_object_params
+from .core.utils import get_interval_division, format_index_by_dimensions, cast_to_bool_dict
+from .core.utils import format_interval_by_limits, give_index_slice_by_limits, update_object_params
 from .core.utils import give_nonzero_limits, mat_structure_to_tight_dataframe_dict
 
 
@@ -47,12 +47,13 @@ class CATSBase(BaseModel, extra=Extra.allow):
     cluster_size_f_Hz: float = 15.0
     cluster_distance_t_sec: float = None
     cluster_distance_f_Hz: float = None
+    cluster_minSNR: float = None
     cluster_fullness: float = Field(0, ge=0.0, le=1.0)
 
     # Minor clustering params
     log_freq_width: float = None
     log_freq_distance: float = None
-    cluster_catalogs: bool = False
+    cluster_catalogs: bool = True
     clustering_multitrace: bool = False
     cluster_size_trace: int = Field(1, ge=1)
     cluster_distance_trace: int = Field(1, ge=1)
@@ -95,6 +96,14 @@ Can be estimated as length of the strongest phases.
 
                 cluster_size_f_Hz : float : minimum cluster size in frequency, in hertz, i.e. minimum frequency width.
 
+                cluster_distance_t_sec : float : neighborhood distance in time for clustering, in seconds. \
+Minimum separation time between two different events.
+
+                cluster_distance_f_Hz : float : neighborhood distance in frequency for clustering, in hertz. \
+Minimum separation frequency width between two different events.
+
+                cluster_minSNR : float [xi(minSNR), inf] : minimum average cluster energy, if None, `minSNR` is used.
+
                 cluster_fullness : float (0, 1] : minimum cluster fullness from its minimum size defined by `size` and \
 `distance` params
 
@@ -108,12 +117,6 @@ applied instead of `cluster_distance_f_Hz`. Default None.
 
                 freq_bandpass_Hz : tuple[float, float] : bandpass frequency range, in hertz, i.e. everything out of \
 the range is zero (e.g. (f_min, f_max)).
-
-                cluster_distance_t_sec : float : neighborhood distance in time for clustering, in seconds. \
-Minimum separation time between two different events.
-
-                cluster_distance_f_Hz : float : neighborhood distance in frequency for clustering, in hertz. \
-Minimum separation frequency width between two different events.
 
                 clustering_multitrace : bool : whether to use multitrace clustering (Location x Frequency x Time). \
 Increase accuracy for multiple arrays of receivers on regular grid. Performs cross-station association.
@@ -180,6 +183,7 @@ The fastest CPU version is 'ssqueezepy', which is default.
         self.cluster_distance_t_len = max(round(self.cluster_distance_t_sec / self.stft_hop_sec), 1)
         self.cluster_distance_f_len = max(round(self.cluster_distance_f_Hz / self.stft_df), 1)
         self.cluster_distance_trace_len = self.cluster_distance_trace
+        self.cluster_minSNR = self.cluster_minSNR or self.minSNR
 
         self.time_edge = int(self.stft_window_len // 2 / self.stft_hop_len)
 
@@ -204,66 +208,49 @@ The fastest CPU version is 'ssqueezepy', which is default.
         """
         update_object_params(self, **params)
 
-    def _apply(self, x, /, finish_on='clustering', verbose=True, full_info=None):
+    def apply_STFT(self, result_container, /):
+        result_container['coefficients'] = self.STFT * result_container['signal']
+        result_container['spectrogram'] = np.abs(result_container['coefficients'])
 
-        result = dict.fromkeys(full_info.keys())
-        result['signal'] = x if full_info['signal'] else None
+    def apply_BEDATE(self, result_container, /):
+        result_container['time_frames'] = get_interval_division(N=result_container['spectrogram'].shape[-1],
+                                                                L=self.stationary_frame_len)
 
-        history = StatusKeeper(verbose=verbose)
+        T, STD, THR = BEDATE_trimming(result_container['spectrogram'], self.frequency_groups_index,
+                                      self.bandpassed_frequency_groups_slice, self.freq_bandpass_slice,
+                                      result_container['time_frames'], self.minSNR, self.time_edge, self.date_Q,
+                                      not self.date_detection_mode)
 
-        # STFT
-        with history(current_process='STFT'):
-            result['coefficients'] = self.STFT * x
-            result['spectrogram'] = np.abs(result['coefficients'])
+        result_container['spectrogram_SNR_trimmed'] = T
+        result_container['noise_std'] = STD
+        result_container['noise_threshold_conversion'] = THR
 
-        del_vals_by_keys(result, full_info, ['coefficients'])
+    def apply_Clustering(self, result_container, /):
+        mc = self.clustering_multitrace
+        q = (self.cluster_distance_trace_len,) * mc + (self.cluster_distance_f_len, self.cluster_distance_t_len)
+        s = (self.cluster_size_trace_len,) * mc + (self.cluster_size_f_len, self.cluster_size_t_len)
+        alpha = self.cluster_fullness
+        log_freq_cluster = (self.log_freq_width, self.log_freq_distance)
 
-        if 'stft' in finish_on.casefold():
-            return result, history
+        result_container['spectrogram_SNR_clustered'] = np.zeros_like(result_container['spectrogram_SNR_trimmed'])
+        result_container['spectrogram_cluster_ID'] = np.zeros(result_container['spectrogram_SNR_trimmed'].shape,
+                                                              dtype=np.uint32)
 
-        # B-E-DATE
-        with history(current_process='B-E-DATE trimming'):
-            result['time_frames'] = get_interval_division(N=result['spectrogram'].shape[-1],
-                                                          L=self.stationary_frame_len)
+        result_container['spectrogram_SNR_clustered'], result_container['spectrogram_cluster_ID'] = \
+            Clustering(result_container['spectrogram_SNR_trimmed'], q=q, s=s,
+                       minSNR=self.cluster_minSNR, alpha=alpha, log_freq_cluster=log_freq_cluster)
 
-            result['spectrogram_SNR_trimmed'], result['noise_std'], result['noise_threshold_conversion'] = \
-                BEDATE_trimming(result['spectrogram'], self.frequency_groups_index,
-                                self.bandpassed_frequency_groups_slice, self.freq_bandpass_slice,
-                                result['time_frames'], self.minSNR, self.time_edge, self.date_Q,
-                                not self.date_detection_mode)
+        if self.cluster_catalogs:
+            result_container['cluster_catalogs'] = ClusterCatalogs(result_container['spectrogram_SNR_clustered'],
+                                                                   result_container['spectrogram_cluster_ID'],
+                                                                   self.stft_frequency, self.stft_hop_sec)
+        else:
+            result_container['cluster_catalogs'] = None
 
-        del_vals_by_keys(result, full_info, ['spectrogram', 'noise_std', 'noise_threshold_conversion'])
-
-        if 'date' in finish_on.casefold():
-            return result, history
-
-        # Clustering
-        with history(current_process='Clustering'):
-            mc = self.clustering_multitrace
-            q = (self.cluster_distance_trace_len,) * mc + (self.cluster_distance_f_len, self.cluster_distance_t_len)
-            s = (self.cluster_size_trace_len,) * mc + (self.cluster_size_f_len, self.cluster_size_t_len)
-            alpha = self.cluster_fullness
-            log_freq_cluster = (self.log_freq_width, self.log_freq_distance)
-
-            result['spectrogram_SNR_clustered'] = np.zeros_like(result['spectrogram_SNR_trimmed'])
-            result['spectrogram_cluster_ID'] = np.zeros(result['spectrogram_SNR_trimmed'].shape, dtype=np.uint32)
-
-            result['spectrogram_SNR_clustered'], result['spectrogram_cluster_ID'] = \
-                Clustering(result['spectrogram_SNR_trimmed'], q=q, s=s,
-                           minSNR=self.minSNR, alpha=alpha, log_freq_cluster=log_freq_cluster)
-
-            if self.cluster_catalogs:
-                result['cluster_catalogs'] = ClusterCatalogs(result['spectrogram_SNR_clustered'],
-                                                             result['spectrogram_cluster_ID'],
-                                                             self.stft_frequency, self.stft_hop_sec)
-            else:
-                result['cluster_catalogs'] = None
-
-        del_vals_by_keys(result, full_info, ['spectrogram_SNR_trimmed',
-                                             'spectrogram_SNR_clustered',
-                                             'spectrogram_cluster_ID'])
-
-        return result, history
+    def apply_func(self, func_name, result_container, status_keeper, process_name=None, **kwargs):
+        process_name = process_name or func_name
+        with status_keeper(current_process=process_name):
+            getattr(self, func_name)(result_container, **kwargs)
 
     @staticmethod
     def _info_keys():
