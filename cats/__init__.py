2e2f531986150b2ab7d4d8026a2c0c7fb2eec5fd
sgrubas <serafimgrubas@gmail.com>
2024-01-27 18:52:30 -0700

    tune module to import

--- cats/__init__.py
+++ cats/__init__.py
@@ -11,6 +11,7 @@ from .denoising import CATSDenoiser, CATSDenoisingResult
 from .data import import_sample_data
 from . import misc
 from . import io
+from . import tune
 from .io import read_data
 from .core import env_variables
 from .core.env_variables import get_max_memory_available_for_cats, set_max_memory_for_cats
