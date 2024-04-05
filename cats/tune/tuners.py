29e75012d341553d66b1db143bda4792e18542f8
sgrubas <serafimgrubas@gmail.com>
2024-04-05 14:38:39 -0600

    modified API for detector and denoiser

--- cats/tune/tuners.py
+++ cats/tune/tuners.py
@@ -6,6 +6,10 @@ import inspect
 from functools import wraps
 
 
+# TODO:
+#   - picks introspection for Detector is needed
+
+
 class BaseScoring(BaseModel):
     operator: Any
     generator_x_and_y: Union[Callable, Any]
@@ -51,7 +55,7 @@ class BaseScoring(BaseModel):
 
 
 class DenoiserScoring(BaseScoring):
-    metric_functions: Union[str, Callable, Dict[str, Callable]] = "rel l2"
+    metric_functions: Union[str, Callable, Dict[str, Callable], Dict[str, str]] = "rel l2"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -77,50 +81,77 @@ class DenoiserScoring(BaseScoring):
             self.metric_functions = {"metric": self.metric_wrapper(self.metric_functions)}
 
         elif isinstance(self.metric_functions, dict):
-            self.metric_functions = {name: self.metric_wrapper(func) for name, func in self.metric_functions.items()}
+            metric_functions = {}
+
+            for name, func_or_str in self.metric_functions.items():
+                if callable(func_or_str):
+                    func = func_or_str
+                elif isinstance(func_or_str, str):
+                    func = linalg_metric_func(func_or_str, axis=-1)
+                else:
+                    raise ValueError(f"Passed metric function {name} is of unknown type {type(func_or_str) = }")
+                metric_functions[name] = self.metric_wrapper(func)
+            self.metric_functions = metric_functions
 
         else:
             raise ValueError(f"Unknown input type for {type(self.metric_functions) = }")
 
 
 class DetectorScoring(BaseScoring):
-    metric_functions: Union[str, Callable, Dict[str, Callable]] = "picks2.5 f0.5"
+    metric_functions: Union[str, Callable, Dict[str, Callable], Dict[str, str]] = "picks2.5 f0.5"
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.set_metric_functions()
 
-    # @staticmethod
-    # def metric_wrapper(func):
-    #     def wrapper(*args, **kwargs):
-    #         return func(args[0], args[1].signal_denoised, *args[2:], **kwargs)
-    #     return wrapper
+    @staticmethod
+    def metric_wrapper(func, picks=True):
+        sig = inspect.signature(func)
+
+        @wraps(func)
+        def wrapper(yi, operator_result, *args, **kwargs):
+            if picks:
+                features = operator_result.picked_features
+                y_pred = np.empty(features.shape, dtype=object)
+                for ind in np.ndindex(*features.shape):
+                    y_pred[ind] = features[ind][:, 0]
+            else:
+                raise NotImplementedError("Metrics not based on pick haven't been implemented yet")
+                # y_pred = ProjectIntervals(detector_result.detected_intervals)
+
+            return func(yi, y_pred, *args[2:], **kwargs)
+
+        wrapper.__signature__ = sig
+
+        return wrapper
 
     def set_metric_functions(self):
         if isinstance(self.metric_functions, str):
             picks = find_word_starting_with(self.metric_functions, "pick", case_insensitive=True)
-            func = binary_metric_func(self.metric_functions)
-
-            def _metric_func(y_true, detector_result):
-                if picks:
-                    features = detector_result.picked_features
-                    y_pred = np.empty(features.shape, dtype=object)
-                    for ind in np.ndindex(*features.shape):
-                        y_pred[ind] = features[ind][:, 0]
-                else:
-                    raise NotImplementedError(f"Metric {self.metric_functions} has not been implemented yet")
-                    # y_pred = ProjectIntervals(detector_result.detected_intervals)
-
-                return func(y_true, y_pred)
-
-            self.metric_functions = {self.metric_functions: _metric_func}
+            func = self.metric_wrapper(binary_metric_func(self.metric_functions), picks)
+            self.metric_functions = {self.metric_functions: func}
 
         elif callable(self.metric_functions):
-            self.metric_functions = {"metric": self.metric_functions}
+            self.metric_functions = self.metric_wrapper(self.metric_functions,
+                                                        picks=True)  # NOTE: introspection for 'picks' !!
 
         elif isinstance(self.metric_functions, dict):
-            pass
+            metric_functions = {}
+            for name, func_or_str in self.metric_functions.items():
+                if callable(func_or_str):
+                    func = func_or_str
+                    picks = True  # NOTE: introspection for 'picks' !!
+
+                elif isinstance(func_or_str, str):
+                    func = binary_metric_func(func_or_str)
+                    picks = find_word_starting_with(func_or_str, "pick", case_insensitive=True)
+
+                else:
+                    raise ValueError(f"Passed metric function {name} is of unknown type {type(func_or_str) = }")
+
+                metric_functions[name] = self.metric_wrapper(func, picks=picks)
+
+            self.metric_functions = metric_functions
 
         else:
             raise ValueError(f"Unknown input type for {type(self.metric_functions) = }")
-
