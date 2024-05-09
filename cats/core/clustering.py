1) Fixed performance bug related to my incorrect implementation of Depth-First Search for clustering/labelling which led to overflow in stack size in `Clustering`. - Previously, stack in DFS was constantly growing (exponentially) because I did not check for visited elements which are already in stack. - Now, the stack in DFS is correctly implemented and the overflow issue has been fixed. It is much more memory efficient now and scales properly with increasing distance of neighbourhood. - Removed old redundant criteria for collecting statistics. 10% faster now.
2) Fixed bug in `_cluster_stats` related to incorrect frequency sizes.
- Slightly simplified `ClusterCatalogs`

cats/core/clustering.py
@@ -14,162 +14,165 @@ from functools import partial
 # ------------------- CLUSTERING ------------------- #
 
 
-def Clustering(SNR, /, q, s, minSNR, alpha, log_freq_cluster):
+def Clustering(SNR, /, q, s, log_freq_cluster):
     """
-        Performs clustering (density-based / neighbor-based) of many trimmed spectrograms in parallel.
+        Performs neighbor-based clustering/labelling of trimmed SNR spectrograms (parallelized).
+        It is based on Depth-First Search (DFS) algorithm. Works analogous to `scipy.ndimage.label`,
+        but this supports `q` > 1 (see below for `q`).
+
         If `len(q) = 2` then 2-dimensional clustering in "Frequency x Time" is used, `SNR.shape=(M, Nf, Nt)`
         If `len(q) = 3` then 3-dimensional clustering in "Trace x Frequency x Time" is used, `SNR.shape=(M, Nc, Nf, Nt)`
 
         Arguments:
-            SNR : np.ndarray (M, Nf, Nt) or (M, Nc, Nf, Nt) : Trimmed spectrogram wherein nonzero elements represent
-                                    SNR values. `M` is number of spectrograms, `Nc` number of traces,
-                                    `Nf` is frequency axis, `Nt` is time axis.
+            SNR : np.ndarray (M, Nf, Nt) or (M, Nc, Nf, Nt) :
+            Trimmed SNR spectrogram where nonzero elements (SNR > 0) will be labelled.
+                                    `M` is number of spectrograms,
+                                    `Nc` is number of traces,
+                                    `Nf` is frequency axis,
+                                    `Nt` is time axis.
             q : tuple(int, 2) / tuple(int, 3) : neighborhood distance for clustering `(q_f, q_t)` or `(q_c, q_f, q_t)`.
                                                 `q_c` for traces, `q_f` for frequency, `q_t` for time.
             s : tuple(int, 2) / tuple(int, 3) : minimum cluster sizes `(s_f, s_t)` or `(s_c, s_f, s_t)`.
                                                 `s_c` for traces, `s_f` for frequency, `s_t` for time.
-            minSNR : float : minimum SNR of clusters
-            alpha : float (0, 1] : minimum cluster fullness
-            log_freq_cluster : tuple(float, float) : logarithmic clustering params (log10) for frequency axis in order:
-                                (cluster_size_f_logHz, cluster_distance_f_logHz). By default, they are 0.0, if not will be used
-                                instead of their original equivalents `s_f` and `q_f` respectively.
+            log_freq_cluster : tuple(float, float) : equivalent to `s_f` and `q_f` (see above), but in
+                                logarithmic scale (log10), for frequency axis only, in the following order:
+                                (cluster_size_f_logHz, cluster_distance_f_logHz).
+                                By default, they are zeros and not used, but if given '> 0',
+                                 then they replace `s_f` and `q_f` respectively.
     """
     func = {2: _ClusteringN2D, 3: _ClusteringN3D}
-    return func[len(q)](SNR, q, s, minSNR, alpha, log_freq_cluster)
+    return func[len(q)](SNR, q, s, log_freq_cluster)
 
 
 IND_ND = lambda n: f'UniTuple(i8, {n})'
-CLUSTER_STAT_SIGNATURE_ND = lambda n: f"Tuple(({IND_ND(n)}, {IND_ND(n)}, i8, f8))"
+CLUSTER_STAT_SIGNATURE_ND = lambda n: f"Tuple(({IND_ND(n)}, {IND_ND(n)}))"
 
 
-@nb.njit([f"{CLUSTER_STAT_SIGNATURE_ND(2)}({IND_ND(2)}, {IND_ND(2)}, f8[:, :], u4[:, :], u4, f8)",
-          f"{CLUSTER_STAT_SIGNATURE_ND(2)}({IND_ND(2)}, {IND_ND(2)}, f4[:, :], u4[:, :], u4, f8)"],
+@nb.njit([f"{CLUSTER_STAT_SIGNATURE_ND(2)}({IND_ND(2)}, {IND_ND(2)}, f8[:, :], i4[:, :], i4, f8)",
+          f"{CLUSTER_STAT_SIGNATURE_ND(2)}({IND_ND(2)}, {IND_ND(2)}, f4[:, :], i4[:, :], i4, f8)"],
          cache=True)
-def iterative_neighbor_search_2D(ind: tuple[int, int], q, SNR, C, cid, log_freq_distance):
+def depth_first_search_2D(ind, q, SNR, C, cid, log_freq_distance):
     q_f, q_t = q
     Nf, Nt = SNR.shape
 
     mins = ind
     maxs = ind
-    n = 0
-    snr = 0.0
 
     stack = [ind]
 
     while stack:
-        curr = stack.pop()
+        # Depth-First Search (DFS, First-In / Last-Out --> stack.pop(-1))
+        # DFS scales better with `q` than Breadth-First (BFS, First-In / First-Out --> stack.pop(0))
+        curr = stack.pop(-1)  # take element to label and scan neighbors
 
-        if C[curr] > 0:
+        if C[curr] == cid:  # check if it was labelled before
             continue
 
-        C[curr] = cid
-        snr += SNR[curr]
-        n += 1
-        mins = (min(mins[0], curr[0]), min(mins[1], curr[1]))
-        maxs = (max(maxs[0], curr[0]), max(maxs[1], curr[1]))
+        C[curr] = cid  # label element
 
+        mins = (min(mins[0], curr[0]), min(mins[1], curr[1]))  # update min indexes
+        maxs = (max(maxs[0], curr[0]), max(maxs[1], curr[1]))  # update max indexes
+
+        # define lookout window to search for neighbors
         i, j = curr
         if log_freq_distance > 0.0:
-            i1 = min(i - 1, round(i * 10**(-log_freq_distance)))
-            i2 = max(i + 1, round(i * 10**log_freq_distance)) + 1
+            i1 = min(i - 1, round(i * 10 ** (-log_freq_distance)))
+            i2 = max(i + 1, round(i * 10 ** log_freq_distance)) + 1
         else:
             i1, i2 = i - q_f, i + q_f + 1
 
         i1, i2 = max(i1, 0), min(i2, Nf)
         j1, j2 = max(j - q_t, 0), min(j + q_t + 1, Nt)
 
+        # search for neighbors to scan next
         for li in range(i1, i2):
             for lj in range(j1, j2):
                 lind = (li, lj)
-                if (SNR[lind] > 0.0) and (C[lind] < 1) and (curr != lind):
-                    stack.append(lind)
+                nonzero = (SNR[lind] > 0.0)  # SNR must be > 0
+                nonvisited = (C[lind] == 0)  # only non-visited elements are added to stack
+                if nonzero and nonvisited:
+                    stack.append(lind)  # put to the stack
+                    C[lind] = -cid  # label as added to the stack
 
-    return mins, maxs, n, snr
+    return mins, maxs
 
 
-@nb.njit([f"Tuple((f8[:, :], u4[:, :]))(f8[:, :], {IND_ND(2)}, {IND_ND(2)}, f8, f8, UniTuple(f8, 2))",
-          f"Tuple((f4[:, :], u4[:, :]))(f4[:, :], {IND_ND(2)}, {IND_ND(2)}, f8, f8, UniTuple(f8, 2))"],
+@nb.njit([f"Tuple((f8[:, :], i4[:, :]))(f8[:, :], {IND_ND(2)}, {IND_ND(2)}, UniTuple(f8, 2))",
+          f"Tuple((f4[:, :], i4[:, :]))(f4[:, :], {IND_ND(2)}, {IND_ND(2)}, UniTuple(f8, 2))"],
          cache=True)
-def _Clustering2D(SNR, q, s, minSNR, alpha, log_freq_cluster):
+def _Clustering2D(SNR, q, s, log_freq_cluster):
     shape = SNR.shape
     log_freq_width, log_freq_distance = log_freq_cluster
 
-    # finding clusters
-    C = np.zeros(shape, dtype=np.uint32)
-    clusters = {}
+    # Finding and labelling clusters
+    C = np.zeros(shape, dtype=np.int32)
+    cluster_size = {}  # remembers cluster sizes
     cid = 1
     for i, j in np.argwhere(SNR):
         ind = (i, j)
         if C[ind] < 1:
-            clusters[cid] = iterative_neighbor_search_2D(ind, q, SNR, C, cid, log_freq_distance)
+            cluster_size[cid] = depth_first_search_2D(ind, q, SNR, C, cid, log_freq_distance)
             cid += 1
 
-    # filtering clusters
+    # Filtering clusters by sizes
     s_f, s_t = s
     k = 1
-    min_cluster_fullness = alpha * s_f * s_t
-    passed_clusters_cid = {}
-    for cid, cluster_stat in clusters.items():
-        mins, maxs, n, snr = cluster_stat
+    cluster_newid = {}  # remembers old cluster IDs to relabel them
+    for cid, (mins, maxs) in cluster_size.items():
         if log_freq_width > 0.0:
             min_frequency_width = np.log10((maxs[0] + 1) / (mins[0] + 1e-8)) >= log_freq_width
         else:
             min_frequency_width = (maxs[0] - mins[0] + 1 >= s_f)
-        cluster_pass = (min_frequency_width and  # min frequency width
-                        (maxs[1] - mins[1] + 1 >= s_t) and  # min time duration
-                        (snr / n >= minSNR) and  # min average cluster energy
-                        (n >= min_cluster_fullness))  # min cluster fullness
-        if cluster_pass:
-            passed_clusters_cid[cid] = k
+        cluster_counted = (min_frequency_width and  # min frequency width
+                           (maxs[1] - mins[1] + 1 >= s_t))  # min time duration
+        if cluster_counted:
+            cluster_newid[cid] = k  # old ID ---> new ID
             k += 1
 
-    # clusters = {new_cid: clusters[old_cid] for old_cid, new_cid in passed_clusters_cid.items()}
-
-    # assigning allowed clusters
+    # Relabelling clusters
     SNRK = np.zeros_like(SNR)
     for i, j in np.argwhere(C):
-        C[i, j] = kid = passed_clusters_cid.get(C[i, j], 0)
-        if kid > 0:
-            SNRK[i, j] = SNR[i, j]
+        ind = (i, j)
+        C[ind] = new_id = cluster_newid.get(C[ind], 0)
+        if new_id > 0:
+            SNRK[ind] = SNR[ind]
 
     return SNRK, C
 
 
 @ReshapeArraysDecorator(dim=3, input_num=1, methodfunc=False, output_num=2, first_shape=True)
-@nb.njit([f"Tuple((f8[:, :, :], u4[:, :, :]))(f8[:, :, :], {IND_ND(2)}, {IND_ND(2)}, f8, f8, UniTuple(f8, 2))",
-          f"Tuple((f4[:, :, :], u4[:, :, :]))(f4[:, :, :], {IND_ND(2)}, {IND_ND(2)}, f8, f8, UniTuple(f8, 2))"],
+@nb.njit([f"Tuple((f8[:, :, :], i4[:, :, :]))(f8[:, :, :], {IND_ND(2)}, {IND_ND(2)}, UniTuple(f8, 2))",
+          f"Tuple((f4[:, :, :], i4[:, :, :]))(f4[:, :, :], {IND_ND(2)}, {IND_ND(2)}, UniTuple(f8, 2))"],
          parallel=True, cache=True)
-def _ClusteringN2D(SNR, q, s, minSNR, alpha, log_freq_cluster):
+def _ClusteringN2D(SNR, q, s, log_freq_cluster):
     SNRK = np.empty_like(SNR)
-    K = np.empty(SNR.shape, dtype=np.uint32)
+    C = np.empty(SNR.shape, dtype=np.int32)
     for i in nb.prange(SNR.shape[0]):
-        SNRK[i], K[i] = _Clustering2D(SNR[i], q, s, minSNR, alpha, log_freq_cluster)
-    return SNRK, K
+        SNRK[i], C[i] = _Clustering2D(SNR[i], q, s, log_freq_cluster)
+    return SNRK, C
 
 
-@nb.njit([f"{CLUSTER_STAT_SIGNATURE_ND(3)}({IND_ND(3)}, {IND_ND(3)}, f8[:, :, :], u4[:, :, :], u4, f8)",
-          f"{CLUSTER_STAT_SIGNATURE_ND(3)}({IND_ND(3)}, {IND_ND(3)}, f4[:, :, :], u4[:, :, :], u4, f8)"],
+@nb.njit([f"{CLUSTER_STAT_SIGNATURE_ND(3)}({IND_ND(3)}, {IND_ND(3)}, f8[:, :, :], i4[:, :, :], i4, f8)",
+          f"{CLUSTER_STAT_SIGNATURE_ND(3)}({IND_ND(3)}, {IND_ND(3)}, f4[:, :, :], i4[:, :, :], i4, f8)"],
          cache=True)
-def iterative_neighbor_search_3D(ind, q, SNR, C, cid, log_freq_distance):
+def depth_first_search_3D(ind, q, SNR, C, cid, log_freq_distance):
+    """ See detailed comments in `depth_first_search_2D` """
+
     q_c, q_f, q_t = q
     Nc, Nf, Nt = SNR.shape
 
     mins = ind
     maxs = ind
-    n = 0
-    snr = 0.0
 
     stack = [ind]
 
     while stack:
-        curr = stack.pop()
+        curr = stack.pop(-1)
 
-        if C[curr] > 0:
+        if C[curr] == cid:
             continue
 
         C[curr] = cid
-        snr += SNR[curr]
-        n += 1
 
         mins = (min(mins[0], curr[0]), min(mins[1], curr[1]), min(mins[2], curr[2]))
         maxs = (max(maxs[0], curr[0]), max(maxs[1], curr[1]), max(maxs[2], curr[2]))
@@ -189,36 +192,37 @@ def iterative_neighbor_search_3D(ind, q, SNR, C, cid, log_freq_distance):
             for li in range(i1, i2):
                 for lj in range(j1, j2):
                     lind = (lk, li, lj)
-                    if (SNR[lind] > 0.0) and (C[lind] < 1) and (curr != lind):
+                    nonzero = (SNR[lind] > 0.0)
+                    nonvisited = (C[lind] == 0)
+                    if nonzero and nonvisited:
                         stack.append(lind)
+                        C[lind] = -cid
 
-    return mins, maxs, n, snr
+    return mins, maxs
 
 
-@nb.njit([f"Tuple((f8[:, :, :], u4[:, :, :]))(f8[:, :, :], {IND_ND(3)}, {IND_ND(3)}, f8, f8, UniTuple(f8, 2))",
-          f"Tuple((f4[:, :, :], u4[:, :, :]))(f4[:, :, :], {IND_ND(3)}, {IND_ND(3)}, f8, f8, UniTuple(f8, 2))"],
+@nb.njit([f"Tuple((f8[:, :, :], i4[:, :, :]))(f8[:, :, :], {IND_ND(3)}, {IND_ND(3)}, UniTuple(f8, 2))",
+          f"Tuple((f4[:, :, :], i4[:, :, :]))(f4[:, :, :], {IND_ND(3)}, {IND_ND(3)}, UniTuple(f8, 2))"],
          cache=True)
-def _Clustering3D(SNR, q, s, minSNR, alpha, log_freq_cluster):
+def _Clustering3D(SNR, q, s, log_freq_cluster):
     shape = SNR.shape
     log_freq_width, log_freq_distance = log_freq_cluster
 
     # finding clusters
-    C = np.zeros(shape, dtype=np.uint32)
-    clusters = {}
+    C = np.zeros(shape, dtype=np.int32)
+    cluster_size = {}
     cid = 1
     for k, i, j in np.argwhere(SNR):
         ind = (k, i, j)
         if C[ind] < 1:
-            clusters[cid] = iterative_neighbor_search_3D(ind, q, SNR, C, cid, log_freq_distance)
+            cluster_size[cid] = depth_first_search_3D(ind, q, SNR, C, cid, log_freq_distance)
             cid += 1
 
     # filtering clusters
     s_c, s_f, s_t = s
     k = 1
-    min_cluster_fullness = alpha * s_c * s_f * s_t
-    passed_clusters_cid = {}
-    for cid, cluster_stat in clusters.items():
-        mins, maxs, n, snr = cluster_stat
+    cluster_newid = {}
+    for cid, (mins, maxs) in cluster_size.items():
         c1, f1, t1 = mins
         c2, f2, t2 = maxs
 
@@ -227,13 +231,11 @@ def _Clustering3D(SNR, q, s, minSNR, alpha, log_freq_cluster):
         else:
             min_frequency_width = (f2 - f1 + 1 >= s_f)
 
-        cluster_pass = ((c2 - c1 + 1 >= s_c) and  # min channel / components / stations
-                        min_frequency_width and  # min frequency width
-                        (t2 - t1 + 1 >= s_t) and  # min time duration
-                        (snr / n >= minSNR) and  # min average cluster energy
-                        (n >= min_cluster_fullness))  # min cluster fullness
-        if cluster_pass:
-            passed_clusters_cid[cid] = k
+        cluster_counted = ((c2 - c1 + 1 >= s_c) and  # min number of traces
+                           min_frequency_width and  # min frequency width
+                           (t2 - t1 + 1 >= s_t))  # min time duration
+        if cluster_counted:
+            cluster_newid[cid] = k
             k += 1
 
     # clusters = {new_cid: clusters[old_cid] for old_cid, new_cid in passed_clusters_cid.items()}
@@ -242,66 +244,79 @@ def _Clustering3D(SNR, q, s, minSNR, alpha, log_freq_cluster):
     SNRK = np.zeros_like(SNR)
     for k, i, j in np.argwhere(C):
         ind = (k, i, j)
-        C[ind] = kid = passed_clusters_cid.get(C[ind], 0)
-        if kid > 0:
+        C[ind] = newid = cluster_newid.get(C[ind], 0)
+        if newid > 0:
             SNRK[ind] = SNR[ind]
 
     return SNRK, C
 
 
 @ReshapeArraysDecorator(dim=4, input_num=1, methodfunc=False, output_num=2, first_shape=True)
-@nb.njit([f"Tuple((f8[:, :, :, :], u4[:, :, :, :]))(f8[:, :, :, :], {IND_ND(3)}, {IND_ND(3)}, f8, f8, UniTuple(f8, 2))",
-          f"Tuple((f4[:, :, :, :], u4[:, :, :, :]))(f4[:, :, :, :], {IND_ND(3)}, {IND_ND(3)}, f8, f8, UniTuple(f8, 2))"],
+@nb.njit([f"Tuple((f8[:, :, :, :], i4[:, :, :, :]))(f8[:, :, :, :], {IND_ND(3)}, {IND_ND(3)}, UniTuple(f8, 2))",
+          f"Tuple((f4[:, :, :, :], i4[:, :, :, :]))(f4[:, :, :, :], {IND_ND(3)}, {IND_ND(3)}, UniTuple(f8, 2))"],
          parallel=True, cache=True)
-def _ClusteringN3D(SNR, q, s, minSNR, alpha, log_freq_cluster):
+def _ClusteringN3D(SNR, q, s, log_freq_cluster):
     SNRK = np.empty_like(SNR)
-    K = np.empty(SNR.shape, dtype=np.uint32)
+    C = np.empty(SNR.shape, dtype=np.int32)
     for i in nb.prange(SNR.shape[0]):
-        SNRK[i], K[i] = _Clustering3D(SNR[i], q, s, minSNR, alpha, log_freq_cluster)
-    return SNRK, K
+        SNRK[i], C[i] = _Clustering3D(SNR[i], q, s, log_freq_cluster)
+    return SNRK, C
 
 
 # ---------------- UTILS ---------------- #
 
-@nb.njit(["f8[:](UniTuple(i8, 2), f8, f8, f8, f8[:], i4[:])",
-          "f8[:](UniTuple(i8, 2), f8, f8, f8, f4[:], i4[:])",
-          "f8[:](UniTuple(i8, 2), f8, f8, f8, f8[:], i8[:])",
-          "f8[:](UniTuple(i8, 2), f8, f8, f8, f4[:], i8[:])"], cache=True)
-def _cluster_stats(shape, df, dt, fmax, arr, inds):
-    freq_inds, time_inds = np.divmod(inds, shape[1])
+@nb.njit(["f8[:](UniTuple(i8, 2), f8, f8, f8[:], i4[:])",
+          "f8[:](UniTuple(i8, 2), f8, f8, f4[:], i4[:])",
+          "f8[:](UniTuple(i8, 2), f8, f8, f8[:], i8[:])",
+          "f8[:](UniTuple(i8, 2), f8, f8, f4[:], i8[:])"], cache=True)
+def _cluster_stats(shape, df, dt, arr, inds):
+    nf, nt = shape
+    freq_inds, time_inds = np.divmod(inds, nt)
 
     t1, t2 = np.min(time_inds) - 0.5, np.max(time_inds) + 0.5
     f1, f2 = np.min(freq_inds) - 0.5, np.max(freq_inds) + 0.5
-    f1, f2 = max(0.0, f1), min(fmax, f2)
+    f1, f2 = max(0.0, f1), min(nf, f2)
 
     snr_sum = np.sum(arr)
     N = len(arr)
-    snr = snr_sum / N
 
     t_center = np.sum(arr * time_inds) / snr_sum * dt
     f_center = np.sum(arr * freq_inds) / snr_sum * df
-    Area = N * df * dt
 
     peak_snr = np.max(arr)
 
-    return np.array([t1 * dt, t2 * dt, t_center, f1 * df, f2 * df, f_center, snr, peak_snr, Area])
+    return np.array([t1 * dt,
+                     t2 * dt,
+                     t_center,
+                     f1 * df,
+                     f2 * df,
+                     f_center,
+                     snr_sum / N,
+                     peak_snr,
+                     N])
 
 
-def clusters_stats_2D(SNR, CID, df, dt, fmax):
+def clusters_stats_2D(SNR, CID, df, dt):
     cids = np.arange(1, CID.max() + 1)
     if len(cids) > 0:
         cluster_stats = ndimage.labeled_comprehension(input=SNR,
                                                       labels=CID,
                                                       index=cids,
-                                                      func=partial(_cluster_stats, SNR.shape, df, dt, fmax),
+                                                      func=partial(_cluster_stats, SNR.shape, df, dt),
                                                       out_dtype=np.ndarray,
                                                       default=np.nan,
                                                       pass_positions=True)
     else:
         cluster_stats = []
-    names = ['Time_start_sec', 'Time_end_sec', 'Time_center_of_mass_sec',
-             'Frequency_start_Hz', 'Frequency_end_Hz', 'Frequency_center_of_mass_Hz',
-             'Average_SNR', 'Peak_SNR', 'Area']
+    names = ['Time_start_sec',
+             'Time_end_sec',
+             'Time_center_of_mass_sec',
+             'Frequency_start_Hz',
+             'Frequency_end_Hz',
+             'Frequency_center_of_mass_Hz',
+             'Average_SNR',
+             'Peak_SNR',
+             'Area']
     df_stats = pd.DataFrame(columns=pd.Index(names, name='Statistics'),
                             index=pd.Index(cids, name='Cluster_ID'),
                             dtype=float)
@@ -310,12 +325,11 @@ def clusters_stats_2D(SNR, CID, df, dt, fmax):
     return df_stats
 
 
-def ClusterCatalogs(SNR, CID, frequency, stft_dt_sec):
-    df = frequency[1] - frequency[0]
+def ClusterCatalogs(SNR, CID, df, dt):
     shape = SNR.shape[:-2]
     clusters_stats = np.empty(shape, dtype=pd.DataFrame)
     for ind in np.ndindex(*shape):
-        clusters_stats[ind] = clusters_stats_2D(SNR[ind], CID[ind], df, stft_dt_sec, frequency[-1])
+        clusters_stats[ind] = clusters_stats_2D(SNR[ind], CID[ind], df, dt)
     return clusters_stats
 
 