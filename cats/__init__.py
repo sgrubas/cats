20f5033c2606d53d47aac7cc9143416f7d21576c
sgrubas <serafimgrubas@gmail.com>
2024-09-19 16:27:03 -0600

    Added version variable "__version__" and default import of "ClusterCatalogs" func

--- cats/__init__.py
+++ cats/__init__.py
@@ -6,9 +6,10 @@ from .core import association
 from .core import utils
 from .core.plottingutils import plot_traces
 from . import metrics
+from .core.clustering import ClusterCatalogs
 from .detection import CATSDetector, CATSDetectionResult
 from .denoising import CATSDenoiser, CATSDenoisingResult
-from .denoising_cwt import CATSDenoiserCWT, CATSDenoiserCWTResult
+from .denoising_cwt import CATSDenoiserCWT, CATSDenoisingCWTResult
 from .data import import_sample_data, load_pretuned_CATS
 from . import misc
 from . import io
@@ -16,3 +17,5 @@ from . import tune
 from .io import read_data
 from .core import env_variables
 from .core.env_variables import get_max_memory_available_for_cats, set_max_memory_for_cats
+
+__version__ = "0.2.0"
