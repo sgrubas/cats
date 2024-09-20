f8ccc0e5b5579927fbd6b093c43dcefefa7cdfb6
sgrubas <serafimgrubas@gmail.com>
2024-09-20 11:54:57 -0600

    dynamic version read

--- cats/__init__.py
+++ cats/__init__.py
@@ -18,4 +18,6 @@ from .io import read_data
 from .core import env_variables
 from .core.env_variables import get_max_memory_available_for_cats, set_max_memory_for_cats
 
-__version__ = "0.2.0"
+import importlib.metadata
+
+__version__ = importlib.metadata.version("cats")
